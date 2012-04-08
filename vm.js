0759f43a722d2036e08c10c5139270342bd57af8
Michael Ballantyne <michael.ballantyne@gmail.com>
2012-04-08 01:20:26 -0600

    better

--- vm.js
+++ vm.js
@@ -86,53 +86,54 @@ var Table = function () {
 };
 
 var Simulation = function (table, results) {
-        this.valid = function () {
-            var valid = true;
-            for (var i = 0; i < table.probs().length; i++) {
-                var p = table.probs()[i];
-                valid = valid && p.xValid() && p.pValid();
-            }
+    var self = this;
+    this.valid = function () {
+        var valid = true;
+        for (var i = 0; i < table.probs().length; i++) {
+            var p = table.probs()[i];
+            valid = valid && p.xValid() && p.pValid();
+        }
 
-            valid = valid && table.totalValid() && is_int(this.trials()) && this.trials() <= 10000;
+        valid = valid && table.totalValid() && is_int(this.trials()) && this.trials() <= 10000;
 
-            return valid;
-        };
+        return valid;
+    };
 
-        this.buildDist = function () {
-            var dist = [];
-            var runningFloor = 0.0;
-            var ps = table.probs();
-            for(var i = 0; i < ps.length; i++) {
-                var p = ps[i];
-                dist.push({floor: runningFloor, value: p.x(), ceiling: runningFloor += p.pOfX()});
-            }
-            return dist;
-        };
+    this.buildDist = function () {
+        var dist = [];
+        var runningFloor = 0.0;
+        var ps = table.probs();
+        for(var i = 0; i < ps.length; i++) {
+            var p = ps[i];
+            dist.push({floor: runningFloor, value: p.x(), ceiling: runningFloor += p.pOfX()});
+        }
+        return dist;
+    };
 
-        this.runSimulation = function (data, e) {
-            if (!this.valid()) {
-                alert("It looks like you haven't entered all the right data for the simulation. Please correct those fields highlighted in red or any you may have left empty.");
-                return;
-            }
-            var dist = this.buildDist();
-            var result = [];
-            for(var j = 0; j < this.trials(); j++) {
-                var rand = Math.random();
-                for(var i = 0; i < dist.length; i++) {
-                    if (rand < dist[i].ceiling && rand >= dist[i].floor) {
-                        result.push(dist[i].value);
-                        break;
-                    }
+    this.runSimulation = function (data, e) {
+        if (!this.valid()) {
+            alert("It looks like you haven't entered all the right data for the simulation. Please correct those fields highlighted in red or any you may have left empty.");
+            return;
+        }
+        var dist = this.buildDist();
+        var result = [];
+        for(var j = 0; j < this.trials(); j++) {
+            var rand = Math.random();
+            for(var i = 0; i < dist.length; i++) {
+                if (rand < dist[i].ceiling && rand >= dist[i].floor) {
+                    result.push(dist[i].value);
+                    break;
                 }
             }
-            results.results(result);
-        };
+        }
+        results.results(result);
+    };
 
-        this.trials = ko.numericObservable(100);
+    this.trials = ko.numericObservable(100);
 
-        this.trialsValid = function () {
-            return is_int(trials()) && trials() <= 10000;
-        };
+    this.trialsValid = function () {
+        return self.trials() <= 10000;
+    };
 };
 
 var Results = function () {
